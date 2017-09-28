6fd5a5beb9986132bed98f15b9a6b5ba640977a4
Alex Grehov <alewmoose@gmail.com>
2017-09-28 22:44:49 +0500

    less: exit on sigint

--- show-lyrics.go
+++ show-lyrics.go
@@ -120,7 +120,7 @@ func saveLyrics(client *http.Client, cacheDir string, si *songinfo.SongInfo) (st
 }
 
 func startLess(filePath string) (*exec.Cmd, error) {
-	cmd := exec.Command("less", "-c", filePath)
+	cmd := exec.Command("less", "-c", "-K", filePath)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
